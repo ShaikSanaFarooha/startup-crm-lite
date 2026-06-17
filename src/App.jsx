21c0fae33b3f946b3461abef2754f1d7a15fe4cb
ShaikSanaFarooha <shaiksana4416@gmail.com>
2026-06-17 16:16:22 +0530

    updated app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import AppRoutes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Toast Notification Provider */}
-      <Toaster 
-        position="top-right"
-        toastOptions={{
-          className: 'text-sm font-medium text-slate-800 bg-white border border-slate-100 rounded-xl shadow-lg',
-          duration: 3000,
-        }}
-      />
       <AppRoutes />
     </BrowserRouter>
   );
 }
 
 export default App;
-
